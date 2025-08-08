990615465e51259ad7e46ce58f3c427eb18aec83
kalaiarasan23 <87535396+kalaiarasan23@users.noreply.github.com>
2025-08-08 09:05:43 +0530

    add back to top button

--- script.js
+++ script.js
@@ -190,3 +190,19 @@ sidebarHeadings.forEach((heading) => {
 
     // Auto-load first section
     sidebarHeadings[0].click();
+
+
+    // back to top button section
+      const backToTopBtn = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  });
+
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
